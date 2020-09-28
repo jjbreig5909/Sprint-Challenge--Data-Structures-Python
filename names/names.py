c32dfa036e59272b55a89c6ee76fb115cee82eac
Jeff Breig <jjbreig@gmail.com>
2020-09-27 22:50:17 -0400

    Stretch goal completed! Much more efficient

--- names/names.py
+++ names/names.py
@@ -69,3 +69,27 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+### Attempting Stretch Goal! ###
+
+start_time = time.time()
+
+f = open('names\list_names_1.txt', 'r')
+names_1 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+f = open("names\list_names_2.txt", 'r')
+names_2 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+duplicates = []  # Return the list of duplicates in this data structure
+set_names_1 = set(names_1)
+set_names_2 = set(names_2)
+# Replace the nested for loops below with your improvements
+duplicates = set_names_1.intersection(set_names_2)
+
+
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
+
